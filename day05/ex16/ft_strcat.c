ca731081eb697d9bce368a01f3dc31f7995c656c
fractalfox01 <fractalfox01@gmail.com>
2018-11-28 16:50:05 -0700

    removing un-needed strlen

--- day05/ex16/ft_strcat.c
+++ day05/ex16/ft_strcat.c
@@ -1,15 +1,3 @@
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strcat(char *dest, char *src)
 {
 	int i;
--- day05/ex17/ft_strncat.c
+++ day05/ex17/ft_strncat.c
@@ -1,15 +1,3 @@
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
-}
-
 char	*ft_strncat(char *dest, char *src, int nb)
 {
 	int i;
